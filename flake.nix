2db4e60b63e78fe9a59a8ff785ad0fa294f70f54
rafaelrc7 <rafaelrc7@gmail.com>
2022-06-05 04:36:33 -0300

    Make repo flake template

--- flake.nix
+++ flake.nix
@@ -1,43 +1,16 @@
 {
-  description = "Nix Flake C Autotools project template";
+  description = "Simple C Autotools flake template";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import nixpkgs { inherit system; };
-        deps = [ ];
-        hello = with pkgs; stdenv.mkDerivation rec {
-          name = "hello";
-          src = ./.;
-          buildInputs = [ ];
-          nativeBuildInputs = deps ++ [ pkg-config autoconf-archive autoreconfHook ];
-        };
-    in {
-
-      defaultPackage = hello;
-
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          autoconf
-          autoconf-archive
-          automake
-          bash
-          binutils
-          gcc
-          gdb
-          gnumake
-          pkg-config
-          valgrind
-        ] ++ deps;
+  outputs = { self }: {
+    templates = {
+      autotools = {
+        path = ./autotools-hello;
+        description = "Simple C Autotools flake template";
       };
-    }
-  );
 
+    };
+
+    defaultTemplate = self.templates.autotools;
+  };
 }
 
